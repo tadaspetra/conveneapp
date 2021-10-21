dea18bcb189ce08459f689e271c5918b81c603bd
Afsal102 <39826977+Afsal102@users.noreply.github.com>
2021-10-21 14:48:18 +0530

    fix(remove) removed singleton instance

--- lib/config_reader.dart
+++ lib/config_reader.dart
@@ -5,21 +5,21 @@ import 'package:flutter/services.dart';
 
 /// - contains the initalization for the firebase emulator suite
 ///- Added only for mobile platforms
-///- will only return a singleton instance
 /// - this will get all the data required to setup from
 /// `./configs/emulator_config.json` if this is not added will use default settings
+/// - HACK: must be added via a provider later when having different endpoints
 class ConfigReader {
-  factory ConfigReader() => _instance;
-  ConfigReader._();
-  static final _instance = ConfigReader._();
   final Map<String, dynamic> _config = {};
   Future<void> initializeConfigReader() async {
     try {
-      final configurationOpitons = await rootBundle.loadString('configs/emulator_config.json');
-      final jsonValue = jsonDecode(configurationOpitons) as Map<String, dynamic>;
+      final configurationOpitons =
+          await rootBundle.loadString('configs/emulator_config.json');
+      final jsonValue =
+          jsonDecode(configurationOpitons) as Map<String, dynamic>;
       _config.addAll(jsonValue);
     } on Exception catch (_) {
-      print('initializeConfigReader | Error | No emulator_config.json found using defaults');
+      print(
+          'initializeConfigReader | Error | No emulator_config.json found using defaults');
     }
   }
 
@@ -29,11 +29,13 @@ class ConfigReader {
 
   /// - if this is not provided default will be used `9099` which firebase
   /// emulators use by default
-  int get authenticationPort => int.tryParse(_config['auth_port'].toString()) ?? 9099;
+  int get authenticationPort =>
+      int.tryParse(_config['auth_port'].toString()) ?? 9099;
 
   /// - if this is not provided default will be used `8080` which firebase
   /// emulators use by default
-  int get firestorePort => int.tryParse(_config['firestore_port'].toString()) ?? 8080;
+  int get firestorePort =>
+      int.tryParse(_config['firestore_port'].toString()) ?? 8080;
 
   ///- builds platform specific hosts
   String _buildIp() {
