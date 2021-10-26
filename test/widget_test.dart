b012d7b4278c44a47b92dc259f1a82ba2860cecc
jsancs05 <javivisan19@gmail.com>
2021-10-26 16:18:22 +0200

    Update widget_test.dart
    Fix for flutter format command

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,7 @@ import 'package:conveneapp/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('empty test for generating coverage',
-      (WidgetTester tester) async {
+  testWidgets('empty test for generating coverage', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
   });
 }
