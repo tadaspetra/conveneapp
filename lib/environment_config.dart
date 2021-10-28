1a15a950ffce55588614ff313cf8f7d4754865cc
Rexyness <39528325+rexyness@users.noreply.github.com>
2021-10-28 12:37:56 +0300

    refactor: moved firebase innit to setUpMain()

--- lib/environment_config.dart
+++ lib/environment_config.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:conveneapp/config_reader.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
 
 /// - this contains the developement environment type `dev`,`prod` and `staging`
 /// if we add in the future.
@@ -22,6 +24,8 @@ class EnvironmentConfig {
 /// HACK: this can used to run logics depending on different environment
 /// - awaiting this in `prod` wont affect the performance of the app
 Future<void> setUpMain(String env) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   if (env == EnvironmentConfig.dev) {
     log('dev');
 
