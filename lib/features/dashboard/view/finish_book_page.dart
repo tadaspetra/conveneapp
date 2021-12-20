63a212767af001555e69625538e7e06f9c638bec
Tadas Petra <60107328+tadaspetra@users.noreply.github.com>
2021-12-20 08:50:04 -0600

    Feature: rating and reviews looking good

--- lib/features/dashboard/view/finish_book_page.dart
+++ lib/features/dashboard/view/finish_book_page.dart
@@ -22,113 +22,137 @@ class FinishBookPage extends ConsumerStatefulWidget {
 class _FinishBookPageState extends ConsumerState<FinishBookPage> {
   final TextEditingController _reviewController = TextEditingController();
   double? rating;
+  late FocusNode _reviewFocus;
+
+  @override
+  void initState() {
+    super.initState();
+    _reviewFocus = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _reviewFocus.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false, //TODO: There is probably a better way to handle this
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Spacer(),
-            SearchBookCard(
-                book: SearchBookModel(
-                    authors: widget.bookModel.authors,
-                    coverImage: widget.bookModel.coverImage,
-                    pageCount: widget.bookModel.pageCount,
-                    title: widget.bookModel.title)),
-            const SafeArea(
-              child: Padding(
-                padding: EdgeInsets.only(left: 20.0),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SafeArea(
+                child: SearchBookCard(
+                    book: SearchBookModel(
+                        authors: widget.bookModel.authors,
+                        coverImage: widget.bookModel.coverImage,
+                        pageCount: widget.bookModel.pageCount,
+                        title: widget.bookModel.title)),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 20.0, top: 20),
                 child: Text(
                   "Rate this book",
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: RatingBar.builder(
-                initialRating: 0,
-                minRating: 1,
-                allowHalfRating: true,
-                glowColor: Palette.niceBlack,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                itemBuilder: (context, _) => const Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                ),
-                onRatingUpdate: (newRating) {
-                  rating = newRating;
-                },
-              ),
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0),
-              child: TextField(
-                controller: _reviewController,
-                maxLines: 6,
-                decoration: const InputDecoration(
-                  hintText: "Add your review of this book",
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: RatingBar.builder(
+                  initialRating: 0,
+                  minRating: 1,
+                  allowHalfRating: true,
+                  glowColor: Palette.niceBlack,
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder: (context, _) => const Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (newRating) {
+                    rating = newRating;
+                    _reviewFocus.requestFocus();
+                  },
                 ),
               ),
-            ),
-            const Spacer(
-              flex: 2,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  'Finish adding book to your history?',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    if (rating == null) {
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(const SnackBar(content: Text("Need to give book a rating")));
-                    } else if (_reviewController.text.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Please add a review")));
-                    } else {
-                      ref.read(currentBooksController.notifier).finishBook(
-                            book: widget.bookModel.copyWith(
-                              rating: rating,
-                              review: _reviewController.text,
-                            ),
-                          );
-                      Navigator.of(context).pop(true);
-                    }
-                  },
-                  child: Text(
-                    'Yes',
-                    style: TextStyle(color: Palette.niceBlue),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 40),
+                child: TextField(
+                  controller: _reviewController,
+                  minLines: 5,
+                  maxLines: null,
+                  focusNode: _reviewFocus,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    hintText: "Add your review of this book",
+                    fillColor: Palette.niceGrey,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Palette.niceGrey, width: 0.0),
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Palette.niceGrey, width: 0.0),
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Palette.niceGrey, width: 0.0),
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(false);
-                  },
-                  child: const Text(
-                    'No',
-                    style: TextStyle(color: Palette.niceBlack),
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Finish adding book to your history?',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            const Spacer(
-              flex: 2,
-            )
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      if (rating == null) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(content: Text("Need to give book a rating")));
+                      } else if (_reviewController.text.isEmpty) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(content: Text("Please add a review")));
+                      } else {
+                        ref.read(currentBooksController.notifier).finishBook(
+                              book: widget.bookModel.copyWith(
+                                rating: rating,
+                                review: _reviewController.text,
+                              ),
+                            );
+                        Navigator.of(context).pop(true);
+                      }
+                    },
+                    child: Text(
+                      'Yes',
+                      style: TextStyle(color: Palette.niceBlue),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                    child: const Text(
+                      'No',
+                      style: TextStyle(color: Palette.niceBlack),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
